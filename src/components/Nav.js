c76fa786937da531504f5252dc4aa939c808bae1
Nicholas Easter <nicholaseaster@Nicholass-MacBook-Pro.local>
2023-06-28 18:40:41 -0700

    uploaded images and tried to format them but am struggling to get all images to be the same relative size

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav>
-            <ul className="flex">
-                <li className="m-2"><Link to="/">Home</Link></li>
-                <li className="m-2"><Link to="/about">About</Link></li>
-                <li className="m-2"><Link to="/content">Content</Link></li>
-                <li className="m-2"><Link to="/resources">Resources</Link></li>
+        <nav className="flex justify-between bg-blue-300 py-1">
+            {/* logo area, main button on nav bar */}
+            <div className=" font-bold text-2xl m-2 hover:text-blue-500"><Link to="/">Robert Nozick</Link></div>
+            {/* the rest of the links */}
+            <ul className="flex text-xl">
+                <li className="m-2 hover:text-blue-500"><Link to="/about">About</Link></li>
+                <li className="m-2 hover:text-blue-500"><Link to="/content">Content</Link></li>
+                <li className="m-2 hover:text-blue-500 mr-4"><Link to="/resources">Resources</Link></li>
             </ul>
         </nav>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,45 @@
 // main page, will have nav bar and general content on robert nozick probably images
 import portrait from "../images/robert_nozick_portrait.jpeg";
-import book from "../images/robert-book.webp";
+import book from "../images/robert-book.jpg";
+import machineImg from "../images/frame_0_delay-s.jpg";
 
 const Home = () => {
+
+    const bio = "Robert Nozick is an American philosopher who is best known for his book 'Anarchy, State, and Utopia', written in 1974. " + 
+                "Robert Nozick was a strong believer that the government should " +
+                "do no more than protect the rights of the individual, and that a person has the right to do whatever they please with their property. He is " + 
+                "also well known for being responsible for the thought experiment called the 'Experience Machine' which challenged hedonism by saying " +
+                "that people value real experiences and the truth more than just pleasure.";
+    
+    //const bookInfo = "";
+
+    //const experienceMachineInfo = "";
 
     return (
         <div>
             <div>
-                <div>
-                    <h1 className="px-6 text-5xl italic bg-gray-300 py-2">Robert Nozick</h1>
-                </div>
                 <ul className="flex flex-col items-center">
-                    <li className="flex">
-                        <div className="p-4"><img src={portrait} /></div>
-                        <div className="p-12"><p>Robert nozick is the guy with the plan and he doesnt like big governemnt and hates the experience machine</p></div>
+                    <li className="flex  mt-6">
+                        <div className="flex-1 m-2"><img src={portrait} alt="painting of Robert Nozick"/></div>
+                        <div className="flex-1 m-2 text-xl flex flex-col justify-center items-center">
+                            <h1 className="text-blue-500 px-6 text-5xl italic py-2 text-center">Robert Nozick</h1>
+                            <p className="p-8">{bio}</p>
+                        </div>
                     </li>
-                    <li><img src={book} /></li>
+                    <li className="flex  mt-6">
+                        <div className="flex-1 m-2 text-xl flex flex-col justify-center items-center">
+                            <h1 className="text-blue-500 px-6 text-5xl italic py-2 text-center">Anarchy, State, and Utopia</h1>
+                            <p className="p-8">{bio}</p>
+                        </div>
+                        <div className="m-2 flex-1"><img src={book} alt="Anarchy, State, and Utopia by Robert Nozick"/></div>
+                    </li>
+                    <li className="flex mt-6">
+                        <div className="m-2 flex-1"><img className="" src={machineImg} alt="experience machine theoretical" /></div>
+                        <div className="m-2 flex-1 text-xl flex flex-col justify-center items-center">
+                            <h1 className="text-blue-500 px-6 text-5xl italic py-2 text-center">Experience Machine</h1>
+                            <p className="p-8">{bio}</p>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
